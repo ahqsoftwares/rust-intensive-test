c4e9f05d1b9810cce30d08bb9d241077f1fbd5f0
AHQSoftwares <ahqsecret@gmail.com>
2023-02-04 11:22:33 +0000

    High Salary

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ use rand::{thread_rng, Rng};
 fn main() {
   let mut unique: Vec<u64> = Vec::new();
   let mut all: Vec<u64> = Vec::new();
+
+  println!("Generating Stuff");
+  let mut rng = thread_rng();
   
-  let mut index: u128 = 0;
+  let mut index: u32 = 0;
   loop {
-    let mut rng = thread_rng();
     let num: u64 = rng.gen();
     all.push(num);
-    if index == u128::MAX {
+    if index == u32::MAX {
       break;
     }
     index += 1;
@@ -17,16 +19,29 @@ fn main() {
 
   println!("Step II");
   
-  for data in all.iter() {
-    let repeats: Vec<&u64> = all.iter().filter(|x| {
+  for data in all.clone().iter() {
+    let cloned_all = all.clone();
+    let repeats: Vec<&u64> = cloned_all.iter().filter(|x| {
       x == &data
     }).collect();
 
     std::thread::sleep_ms(2000);
     
     if repeats.len() == 1 {
+      let new_all: Vec<u64> = all.clone().iter().filter(|x| {
+        x == &data
+      }).map(|value| {
+        u64::from(value.clone().to_owned())
+      }).collect();
+      
+      all = new_all.clone();
       unique.push(data.clone());
     }
+
+    println!("Len: {}", all.len());
+    
+    drop(repeats);
+    drop(cloned_all);
   }
 
   println!("Total unique: {}", unique.len());
